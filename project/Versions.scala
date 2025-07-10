3498b285420c2e0c5b48850b27b34f706e945b39
xuwei-k <6b656e6a69@gmail.com>
2025-07-10 09:34:35 +0900

    remove scala-collection-compat

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,9 @@
 object Versions {
-  val commonsCsv       = "1.13.0"
-  val collectionCompat = "2.12.0"
-  val jacksonCsv       = "2.18.4"
-  val jodaTime         = "2.13.0"
-  val kantanCodecs     = "0.6.0"
-  val opencsv          = "5.10"
-  val scalaCsv         = "2.0.0"
-  val univocity        = "2.9.1"
+  val commonsCsv   = "1.13.0"
+  val jacksonCsv   = "2.18.4"
+  val jodaTime     = "2.13.0"
+  val kantanCodecs = "0.6.0"
+  val opencsv      = "5.10"
+  val scalaCsv     = "2.0.0"
+  val univocity    = "2.9.1"
 }
