f896f9be4e402fdf834e33adf3483e99ac45a722
xuwei-k <6b656e6a69@gmail.com>
2025-07-10 14:32:06 +0900

    delete project/Versions.scala

--- project/Versions.scala
+++
@@ -1,9 +0,0 @@
-object Versions {
-  val commonsCsv   = "1.14.0"
-  val jacksonCsv   = "2.19.1"
-  val jodaTime     = "2.14.0"
-  val kantanCodecs = "0.6.0"
-  val opencsv      = "5.11.2"
-  val scalaCsv     = "2.0.0"
-  val univocity    = "2.9.1"
-}
