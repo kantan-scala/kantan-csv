50b689c7cbdf5cdc1cdd5ba7afe1bfc896c1b9e9
kenji yoshida <6b656e6a69@gmail.com>
2025-07-23 07:38:05 +0900

    Update DerivedRowDecoderTests.scala

--- generic/shared/src/test/scala/kantan/csv/generic/DerivedRowDecoderTests.scala
+++ generic/shared/src/test/scala/kantan/csv/generic/DerivedRowDecoderTests.scala
@@ -19,11 +19,13 @@ package kantan.csv.generic
 import kantan.csv.CellDecoder
 import kantan.csv.DecodeError
 import kantan.csv.RowDecoder
+import org.scalatest.EitherValues
 import org.scalatest.funsuite.AnyFunSuite
 
-@SuppressWarnings(Array("scalafix:DisableSyntax.var"))
-class DerivedRowDecoderTests extends AnyFunSuite {
-  test("does not call all CellDecoder") {
+@SuppressWarnings(Array("scalafix:DisableSyntax.isInstanceOf"))
+class DerivedRowDecoderTests extends AnyFunSuite with EitherValues {
+  @SuppressWarnings(Array("scalafix:DisableSyntax.var"))
+  private class Setup {
     var a1Call, a2Call, a3Call = 0
     case class A1(x: Int)
     object A1 {
@@ -47,9 +49,36 @@ class DerivedRowDecoderTests extends AnyFunSuite {
       }
     }
     case class B(a1: A1, a2: A2, a3: A3)
+  }
 
-    assert((a1Call, a2Call, a3Call) == (0, 0, 0))
-    assert(RowDecoder[B].decode(Seq("10", "invalid", "30")).isLeft)
-    assert((a1Call, a2Call, a3Call) == (1, 1, 0))
+  test("does not call all CellDecoder") {
+    new Setup {
+      assert((a1Call, a2Call, a3Call) == (0, 0, 0))
+      assert(RowDecoder[B].decode(Seq("10", "invalid", "30")).left.value.isInstanceOf[DecodeError.TypeError])
+    }
+  }
+
+  test("OutOfBounds") {
+    new Setup {
+      assert(RowDecoder[B].decode(Nil).left.value.isInstanceOf[DecodeError.OutOfBounds])
+      assert(RowDecoder[B].decode(Seq("10")).left.value.isInstanceOf[DecodeError.OutOfBounds])
+      assert(RowDecoder[B].decode(Seq("10", "20")).left.value.isInstanceOf[DecodeError.OutOfBounds])
+    }
+  }
+
+  test("too many values") {
+    new Setup {
+      val expect = Right(B(A1(10), A2(20), A3(30)))
+      assert(RowDecoder[B].decode(Seq("10", "20", "30", "40")) == expect)
+      assert(RowDecoder[B].decode(Seq("10", "20", "30", "40", "50")) == expect)
+      assert(RowDecoder[B].decode(Seq("10", "20", "30", "40", "50", "60")) == expect)
+      assert(RowDecoder[B].decode(Seq("10", "20", "30", "40", "50", "aaa")) == expect)
+    }
+  }
+
+  test("`OutOfBounds` vs `TypeError`") {
+    new Setup {
+      assert(RowDecoder[B].decode(Seq("10", "aaa")).left.value.isInstanceOf[DecodeError.TypeError])
+    }
   }
 }
